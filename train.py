abb0ed440046db9c4ca5fe5c83354414d5428307
Meysam Shamsi <meysam.shamsi@univ-lemans.fr>
2025-12-17 17:49:16 +0100

    cleaning

--- train.py
+++ train.py
@@ -5,15 +5,23 @@ from agents.rl_agent import train_rl_agent_ppo_mlp,train_rl_agent_a2c_mlp,train_
 
 for level in [1,2,3,4]:
 
+    print(f"\n\n ----- LEVEL {level} -----")
+
+    print(f"\t ----- LEVEL {level} / TD3 -----")
+
     env = ShepherdEnv(level=level)
-    model = train_rl_agent_td3_mlp(env, timesteps=1000)
+    model = train_rl_agent_td3_mlp(env, timesteps=1000000)
     model.save(f"models/shepherd_level{level}_td3_mlp")
 
-    env = ShepherdEnv(level=level)
-    model = train_rl_agent_a2c_mlp(env, timesteps=1000)
-    model.save(f"models/shepherd_level{level}_a2c_mlp")
+    print(f"\t ----- LEVEL {level} / A2C -----")
 
     env = ShepherdEnv(level=level)
-    model = train_rl_agent_ppo_mlp(env, timesteps=1000)
+    model = train_rl_agent_a2c_mlp(env, timesteps=1000000)
+    model.save(f"models/shepherd_level{level}_a2c_mlp")
+
+    print(f"\t ----- LEVEL {level} / PPO -----")
+
+    env = ShepherdEnv(level=level)
+    model = train_rl_agent_ppo_mlp(env, timesteps=1000000)
     model.save(f"models/shepherd_level{level}_ppo_mlp")
 
