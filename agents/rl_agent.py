935c2ac5bf10e8eeb8d51861a2d772575971b797
Meysam Shamsi <meysam.shamsi@univ-lemans.fr>
2025-12-17 17:55:38 +0100

    cleaning

--- agents/rl_agent.py
+++ agents/rl_agent.py
@@ -2,17 +2,17 @@ from stable_baselines3 import PPO,A2C,TD3
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
 import numpy as np
 
-def train_rl_agent_ppo_mlp(env, timesteps=50000):
+def train_rl_agent_ppo_mlp(env, timesteps=500000):
     model = PPO("MlpPolicy", env, verbose=1)
     model.learn(total_timesteps=timesteps,log_interval=500)
     return model
 
-def train_rl_agent_a2c_mlp(env, timesteps=50000):
+def train_rl_agent_a2c_mlp(env, timesteps=500000):
     model = A2C("MlpPolicy", env, verbose=1)
     model.learn(total_timesteps=timesteps,log_interval=500)
     return model
 
-def train_rl_agent_td3_mlp(env, timesteps=50000):
+def train_rl_agent_td3_mlp(env, timesteps=500000):
     n_actions = env.action_space.shape[-1]
     action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
     model = TD3("MlpPolicy", env,action_noise=action_noise, verbose=1)
--- train.py
+++ train.py
@@ -9,19 +9,28 @@ for level in [1,2,3,4]:
 
     print(f"\t ----- LEVEL {level} / TD3 -----")
 
-    env = ShepherdEnv(level=level)
-    model = train_rl_agent_td3_mlp(env, timesteps=1000000)
-    model.save(f"models/shepherd_level{level}_td3_mlp")
+    try:
+        env = ShepherdEnv(level=level)
+        model = train_rl_agent_td3_mlp(env, timesteps=1000000)
+        model.save(f"models/shepherd_level{level}_td3_mlp")
+    except:
+         print(f"!!! LEVEL {level} / TD3  Filed")
 
     print(f"\t ----- LEVEL {level} / A2C -----")
 
-    env = ShepherdEnv(level=level)
-    model = train_rl_agent_a2c_mlp(env, timesteps=1000000)
-    model.save(f"models/shepherd_level{level}_a2c_mlp")
+    try:
+        env = ShepherdEnv(level=level)
+        model = train_rl_agent_a2c_mlp(env, timesteps=1000000)
+        model.save(f"models/shepherd_level{level}_a2c_mlp")
+    except:
+         print(f"!!! LEVEL {level} / A2C  Filed")
 
     print(f"\t ----- LEVEL {level} / PPO -----")
-
-    env = ShepherdEnv(level=level)
-    model = train_rl_agent_ppo_mlp(env, timesteps=1000000)
-    model.save(f"models/shepherd_level{level}_ppo_mlp")
+    
+    try:
+        env = ShepherdEnv(level=level)
+        model = train_rl_agent_ppo_mlp(env, timesteps=1000000)
+        model.save(f"models/shepherd_level{level}_ppo_mlp")
+    except:
+         print(f"!!! LEVEL {level} / PPO  Filed")
 
