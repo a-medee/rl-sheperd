551f11c4ac92485251fc376ca8bb95f9114d3050
MeysamSh <meysam.shamsi@univ-lemans.fr>
2026-01-06 15:19:26 +0100

    minor bug

--- train.py
+++ train.py
@@ -7,28 +7,25 @@ for level in [1,2,3,4]:
 
     print(f"\n\n ----- LEVEL {level} -----")
 
-    print(f"\t ----- LEVEL {level} / TD3 -----")
-
     try:
         env = ShepherdEnv(level=level)
+        print(f"\t ----- LEVEL {level} / TD3  (#sheep:{env.n_sheep})-----")
         model = train_rl_agent_td3_mlp(env, timesteps=1000000)
         model.save(f"models/shepherd_level{level}_td3_mlp")
     except:
          print(f"!!! LEVEL {level} / TD3  Filed")
 
-    print(f"\t ----- LEVEL {level} / A2C -----")
-
     try:
         env = ShepherdEnv(level=level)
+        print(f"\t ----- LEVEL {level} / A2C (#sheep:{env.n_sheep})-----")
         model = train_rl_agent_a2c_mlp(env, timesteps=1000000)
         model.save(f"models/shepherd_level{level}_a2c_mlp")
     except:
          print(f"!!! LEVEL {level} / A2C  Filed")
-
-    print(f"\t ----- LEVEL {level} / PPO -----")
     
     try:
         env = ShepherdEnv(level=level)
+        print(f"\t ----- LEVEL {level} / PPO (#sheep:{env.n_sheep})-----")
         model = train_rl_agent_ppo_mlp(env, timesteps=1000000)
         model.save(f"models/shepherd_level{level}_ppo_mlp")
     except:
