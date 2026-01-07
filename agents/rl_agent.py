7f888251c6859e1ca25040597fa217323227558a
MeysamSh <meysam.shamsi@univ-lemans.fr>
2026-01-07 17:12:32 +0100

    fix minor bug train

--- agents/rl_agent.py
+++ agents/rl_agent.py
@@ -21,7 +21,7 @@ def train_rl_agent_ppo_mlp(env,eval_env, timesteps=500000):
     return model
 
 def train_rl_agent_a2c_mlp(env, eval_env, timesteps=500000):
-    model = A2C("MlpPolicy", env, verbose=1)
+    model = A2C("MlpPolicy", env, verbose=1,tensorboard_log="logs/a2c")
     eval_callback = EvalCallback(eval_env, best_model_save_path='./logs/a2c/',
                                 log_path='./logs/a2c/', eval_freq=5000,
                                 deterministic=True, render=False)
@@ -31,7 +31,7 @@ def train_rl_agent_a2c_mlp(env, eval_env, timesteps=500000):
 def train_rl_agent_td3_mlp(env, eval_env, timesteps=500000):
     n_actions = env.action_space.shape[-1]
     action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
-    model = TD3("MlpPolicy", env, action_noise=action_noise, verbose=1)
+    model = TD3("MlpPolicy", env, verbose=1,tensorboard_log="logs/td3", action_noise=action_noise)
     eval_callback = EvalCallback(eval_env, best_model_save_path='./logs/td3/',
                                 log_path='./logs/td3/', eval_freq=5000,
                                 deterministic=True, render=False)
