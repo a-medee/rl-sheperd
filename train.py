1d8baf314675e397331ed7fb51a5db8847f10b05
MeysamSh <meysam.shamsi@univ-lemans.fr>
2026-01-07 17:06:51 +0100

    fix Sparse Reward

--- train.py
+++ train.py
@@ -1,9 +1,6 @@
 from envs.shepherd_env import ShepherdEnv
 from agents.rl_agent import train_rl_agent_ppo_mlp,train_rl_agent_a2c_mlp,train_rl_agent_td3_mlp
 
-<<<<<<< HEAD
-
-
 
 for level in [1,2,3,4]:
 
@@ -11,10 +8,6 @@ for level in [1,2,3,4]:
     # eval_env = ShepherdEnv(level=level, n_sheep=5)
     # print(f"\t ----- LEVEL {level} / PPO (#sheep:{env.n_sheep})-----")
     # model = train_rl_agent_ppo_mlp(env,eval_env, timesteps=1000000)
-=======
-for level in [1,2,3,4]:
-
->>>>>>> HEAD@{1}
 
     print(f"\n\n ----- LEVEL {level} -----")
 
